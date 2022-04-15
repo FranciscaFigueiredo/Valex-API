8707c01728a8115aa4307faa139ea5091c27a2c5
Francisca <fraan928@gmail.com>
2022-04-15 02:47:20 -0300

    gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-
 
 export {
     app,
